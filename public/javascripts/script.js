ce410e089904815777e3b5855756f506a32d72da
yparmar2080 <61891116+yparmar123@users.noreply.github.com>
2021-11-09 20:45:18 -0500

    next and previous works

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -1,13 +1,40 @@
 let currentComic = {};
-let page = 1;
+let maxComicNo;
 let img = document.querySelector("#comic");
+let date = document.querySelector("#date");
+let next = document.querySelector("#next");
+let previous = document.querySelector("#previous");
+let comicNo = document.querySelector("#comic-number");
+let transcript = document.querySelector("#transcript");
+let title = document.querySelector("#title");
 
-const loadComicData = () => {
-  fetch("comic/500")
+const fetchComic = (comicNum) => {
+  fetch(`/comic/${comicNum}`)
     .then((res) => {
       res.json().then((data) => {
         currentComic = data;
+        console.log(currentComic);
         img.setAttribute("src", currentComic.img);
+        comicNo.setAttribute("value", currentComic.num);
+        title.innerHTML = `${currentComic.safe_title}`;
+        date.innerHTML = `${currentComic.month}-${currentComic.day}-${currentComic.year}`;
+        next.addEventListener("click", nextComic);
+        previous.addEventListener("click", previousComic);
+        if (comicNum === "") {
+          maxComicNo = currentComic.num;
+        }
+
+        if (maxComicNo === currentComic.num) {
+          previous.disabled = true;
+        } else {
+          previous.disabled = false;
+        }
+
+        if (currentComic.num === 1) {
+          next.disabled = true;
+        } else {
+          next.disabled = false;
+        }
       });
     })
     .catch((err) => {
@@ -15,6 +42,24 @@ const loadComicData = () => {
     });
 };
 
+const loadHomePage = () => {
+  fetchComic("");
+};
+
+const loadComic = (comicNum) => {
+  fetchComic(comicNum);
+};
+
+const previousComic = () => {
+  fetchComic(currentComic.num + 1);
+};
+
+const nextComic = () => {
+  fetchComic(currentComic.num - 1);
+};
+
 window.onload = function () {
-  loadComicData();
+  loadHomePage();
+  previous.disabled = true;
+  maxComicNo = currentComic.num;
 };
